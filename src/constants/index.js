6e665f472edd3028d8e91c817887468fec7cba70
apurba-striker <83547878+apurba-striker@users.noreply.github.com>
2023-07-05 22:32:40 +0530

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,122 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Service",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Innovation",
+    icon: web,
+  },
+  {
+    title: "Financial Inclusion",
+    icon: mobile,
+  },
+  {
+    title: "Security and Trust",
+    icon: backend,
+  },
+  {
+    title: "Personalization",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+  },
+];
+
+const experiences = [
+  {
+    title: "Digital Banking Reinvented",
+    
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "1",
+    points: [
+      "Say goodbye to traditional banking hassles. Our digital banking solutions empower you to manage your accounts, payments, and investments with ease, right at your fingertips.",
+    ],
+  },
+  {
+    title: "Smart Investing",
+   
+    icon: tesla,
+    iconBg: "#E6DEDD",
+    date: "2",
+    points: [
+      "Experience the power of AI-driven investment strategies that align with your goals and risk tolerance. Invest confidently with our cutting-edge portfolio management tools.",
+    ],
+  },
+  {
+    title: "Seamless Payments",
+   
+    icon: shopify,
+    iconBg: "#383E56",
+    date: "3",
+    points: [
+      "From contactless payments to cross-border transactions, our platform ensures your money moves securely and swiftly, saving you time and effort.",
+    ],
+  },
+  {
+    title: "Financial Wellness",
+   
+    icon: meta,
+    iconBg: "#E6DEDD",
+    date: "4",
+    points: [
+      "Achieve financial harmony with our comprehensive financial planning tools, budgeting insights, and real-time spending analysis.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    
+  },
+];
+
+const projects = [
+  {
+    
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
